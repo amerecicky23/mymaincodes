1da156a7519bdf5ee4bb185530ade1db77e3ecd7
amerecicky23 <44478149+amerecicky23@users.noreply.github.com>
2020-11-01 15:19:17 -0500

    Car Game app made in C# to compare car speeds

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CarGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter car speed 1:");
+            int speed1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter car speed 2:");
+            int speed2 = Convert.ToInt32(Console.ReadLine());
+
+            if(speed1 > speed2)
+            {
+                Console.WriteLine("Car 1 wins");
+            }
+            else
+            {
+                Console.WriteLine("Car 2 wins");
+            }
+        }
+    }
+}
